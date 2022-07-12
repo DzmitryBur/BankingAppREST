19e00bfe2349312e026b02650b88a3dfb0c631a5
Dzmitry Buracheuski <dmitry.burr@gmail.com>
2022-07-12 19:01:38 +0300

    fixed PUT method for Transaction

--- src/main/java/com/cot/bankingapprest/model/User.java
+++ src/main/java/com/cot/bankingapprest/model/User.java
@@ -1,9 +1,11 @@
 package com.cot.bankingapprest.model;
 
 
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
 @Component
+@Data
 public class User {
 
     private long id;
@@ -13,64 +15,5 @@ public class User {
     private String login;
     private String password;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", industry='" + industry + '\'' +
-                ", residency='" + residency + '\'' +
-                ", login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getIndustry() {
-        return industry;
-    }
-
-    public void setIndustry(String industry) {
-        this.industry = industry;
-    }
-
-    public String getResidency() {
-        return residency;
-    }
-
-    public void setResidency(String residency) {
-        this.residency = residency;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
-
